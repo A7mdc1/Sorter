460592884f2a2a50e3a39d6e00efcffde51463e1
Badr Alshlowi <badralshlowi@MacBook-Pro-2.local>
2023-09-25 18:04:02 +0300

    Edited the insertion method

--- Sorter.java
+++ Sorter.java
@@ -31,13 +31,18 @@ public class Sorter{
             int key = arr[i];
             int j = i - 1;
             
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
-                j = j - 1;
+                j--;
             }
             
             arr[j + 1] = key;
         }
     }
+
+
+    
 
 }
